7e991d4ed969d62240bc862c832bb2dd3b3f6ded
trantu-dev <trananhtu@protonmail.ch>
2020-02-24 20:15:10 +0700

    remove all console log

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,7 +4,6 @@ import CollectionsOverview from '../../components/collections-overview/collectio
 import CollectionPage from '../collection/collection.components';
 
 const ShopPage = ({ match }) => {
-  console.log(match);
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
